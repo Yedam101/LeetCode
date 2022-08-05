92a4df4bd96b87faa3e256638a667aa9dd0dd939
Yedam101 <109948144+Yedam101@users.noreply.github.com>
2022-08-05 18:57:14 +0900

    Time: 1565 ms (29.15%), Space: 14.9 MB (95.53%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        hash = {}
-        
-        for i, num in enumerate(nums):
-            hash[num] = i
-            
         pairs = [target - x for x in nums]
         
         for i, pair in enumerate(pairs):
-            if pair in hash and hash[pair] != i:
-                return [i, hash[pair]]
-            
+            if pair in nums and i != nums.index(pair):
+                return [i, nums.index(pair)]
+        
